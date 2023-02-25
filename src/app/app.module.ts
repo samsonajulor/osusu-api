3355da8b382adefcc2456967fc4e0b6c05d7f18d
Samson Ajulor <samsonajulor@gmail.com>
2023-02-25 16:33:59 +0100

    updated the pj

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,48 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe, MiddlewareConsumer } from '@nestjs/common';
+import { APP_PIPE, APP_FILTER } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AllExceptionsFilter } from '../common/utilities/all-exceptions.filter';
+
+/** Modules */
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cookieSession = require('cookie-session');
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      // envFilePath: `.env.${process.env.NODE_ENV}`,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot(),
+  ],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private configService: ConfigService) {}
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: [this.configService.get('COOKIE_KEY')],
+        }),
+      )
+      .forRoutes('*');
+  }
+}
